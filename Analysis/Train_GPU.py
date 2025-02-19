5405e409c2e260a20c84358ae019d76e313b25ab
DanGeospatial <daniel.nelson.ca@gmail.com>
2025-02-19 11:37:41 -0500

    create project structure and import needed scripts

---
+++ Analysis/Train_GPU.py
@@ -0,0 +1,118 @@
+"""
+Set up for GPU testing
+Calculate SHAP values on GPU for massive performance boost
+"""
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error  # , root_mean_squared_error
+from math import sqrt
+import xgboost as xgb
+import pandas as pd
+import shap
+import numpy as np
+
+v3_059 = "D:/Wildfire_Datasets_v7/perm_full_wildfire_dataset.csv"
+
+train_ratio = 0.60
+validation_ratio = 0.20
+test_ratio = 0.20
+
+print("Reading Dataset")
+df = pd.read_csv(v3_059)
+
+left_upper_rh = df['1_clm'].mul(17.269)
+left_bottom_rh = df['1_clm'].add(273.3)
+right_upper_rh = df['2_clm'].mul(17.269)
+right_bottom_rh = df['2_clm'].add(237.3)
+
+left_bracket_rh = left_upper_rh.div(left_bottom_rh)
+right_bracket_rh = right_upper_rh.div(right_bottom_rh)
+
+right_rh = left_bracket_rh.sub(right_bracket_rh)
+rh = np.exp(right_rh)
+rh_f = rh.to_frame('rh')
+
+dfc = pd.concat([df, rh_f], axis=1)
+
+y = dfc["dNBR"]
+x = dfc[["pre", "LC", "46_clm", "tpi", "43_clm", "34_clm", "45_clm", "VS", "WE", "24_clm", "37_clm", "49_clm",
+         "Aspect", "33_clm", "39_clm", "38_clm", "40_clm", "50_clm", "7_clm", "32_clm", "44_clm", "36_clm", "rh",
+         "DAH"]]
+
+print("Splitting Data")
+# train is now 60% of the entire data set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1 - train_ratio, random_state=32)
+
+# test is now 20% of the initial data set
+# validation is now 20% of the initial data set
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test,
+                                                test_size=test_ratio / (test_ratio + validation_ratio),
+                                                random_state=32)
+
+print("Define XGBRegressor")
+
+params = {
+    'min_split_loss': 0,
+    'learning_rate': 0.01,
+    'max_depth': 10,
+    'min_child_weight': 1,
+    'subsample': 0.7059719473304258,
+    'colsample_bytree': 0.8994514591758284,
+    'colsample_bylevel': 0.8904199797234215,
+    'colsample_bynode': 0.8754449733223327,
+    'reg_lambda': 0.016916708991554074,
+    'reg_alpha': 0.0011336609763898625,
+    'random_state': 32,
+    'objective': 'binary:logistic',
+    'tree_method': 'hist',
+    'device': 'cuda',
+    'sampling_method': 'gradient_based'
+}
+
+dtrain = xgb.DMatrix(x_train, y_train)
+d_val = xgb.DMatrix(x_val, y_val)
+
+
+# Input optimal parameters from search
+# verbose_eval can crash JupyterLab - verbose_eval=False
+regressor = xgb.train(params=params, dtrain=dtrain, num_boost_round=5000, early_stopping_rounds=10,
+                      evals=[(dtrain, "train"), (d_val, "val")])
+
+print("Save Model")
+print(regressor.best_iteration)
+# regressor.save_model("D:/Wildfire_Results_v7/xgb_fine_tune.json")
+
+predictions = regressor.predict(d_val)
+
+# Calculate MSE
+mse = mean_squared_error(y_val, predictions)
+print('Mean Squared Error: ', mse)
+
+# Val %VE (Should be same as r2)
+VEV = (1 - (mse / (y_val.var()))) * 100
+print('%VE: ', VEV)
+
+# Calculate RMSE
+rmse = sqrt(mse)
+print('Root Mean Squared Error: ', rmse)
+
+# Val %RMSE
+P_RMSE = (abs(rmse / (y_val.mean())) * 100)
+print('% RMSE: ', P_RMSE)
+"""
+print("Run Explainer")
+shap_values = regressor.predict(d_val, pred_contribs=True)
+
+print("Format SHAP Values")
+exp = shap.Explanation(shap_values[:, :-1], data=x_val, feature_names=x_val.columns)
+exp_values = exp.values
+print("Save SHAP Values")
+
+# Export values to investigate
+explain_export = pd.DataFrame({
+    'row_id': x_val.index.values.repeat(x_val.shape[1]),
+    'feature': x_val.columns.to_list() * x_val.shape[0],
+    'feature_value': x_val.values.flatten(),
+    'shap_values': exp_values.flatten()
+})
+explain_export.to_csv("gpu_shap_df_lc.csv")
+"""
---
+++ Data/Dataset.py
@@ -0,0 +1,109 @@
+"""
+Remove features to improve accuracy and efficiency by dropping features that are add error, etc.
+
+Copyright (C) 2024 Daniel Nelson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+import numpy as np
+import pandas as pd
+from os import scandir
+
+from pandas import isnull
+
+input_path = "D:/Wildfire_Compiled_v8/"
+datasets_path = "D:/Wildfire_Datasets_v8/perm_full_wildfire_dataset.csv"
+perm_path = "D:/Wildfire_Datasets_v8/perm_wildfire_dataset.csv"
+reduced_path = "D:/Wildfire_Datasets_v8/wildfire_dataset.csv"
+combined_ds = []
+
+# Merge all the csv files into one
+with scandir(input_path) as pth:
+    for file in pth:
+        ds = pd.read_csv(file)
+        combined_ds.append(ds)
+df = pd.concat(combined_ds)
+# df.to_csv(datasets_path, index=False)
+
+# Print column headers
+print(df.columns)
+
+# Remove columns that are not needed
+error_columns = ['Unnamed: 0', 'spatial_ref']
+df.drop(error_columns, axis=1, inplace=True)
+
+# Drop irrelevant features, i.e. lake temperature
+irrelevant_columns = ['8_clm', '9_clm', '10_clm', '11_clm', '12_clm', '13_clm', '14_clm', '15_clm', '16_clm', '17_clm',
+                      '18_clm', '19_clm', '20_clm', '21_clm', '22_clm', '41_clm']
+df.drop(irrelevant_columns, axis=1, inplace=True)
+
+df.dropna(inplace=True)
+
+for column in df:
+    df = df[df[column] != np.nan]
+    df = df[df[column] != np.nan]
+
+# Remove no data rows
+df = df[df.LC != 255]
+df = df[df.LC != 0]
+
+# Remove Unclassified data rows
+df = df[df.LC != 0]
+
+# Remove water data rows
+df = df[df.LC != 20]
+
+# Remove glacier/snow data rows
+df = df[df.LC != 31]
+
+# Remove rock data rows
+df = df[df.LC != 32]
+
+# Remove unburned rows
+df = df[df.dNBR < -100]
+df.dNBR = df.dNBR.multiply(-1)
+print(df['dNBR'].mean())
+
+# reduced with xy
+todrop = ['x', 'y', '24_clm', '25_clm', '26_clm', '27_clm', '29_clm', '2_clm', '32_clm', '37_clm', 'twi',
+          '3_clm', '40_clm', '42_clm', '44_clm', '45_clm', '4_clm', '5_clm', '6_clm', '7_clm', '49_clm']
+perm = df.drop(todrop, axis=1)
+
+permutated = perm.sample(frac=1, random_state=32).reset_index(drop=True)
+# print(permutated.columns)
+
+tdrop = ['1_clm', '23_clm', '28_clm', '30_clm', '31_clm', '33_clm', '34_clm', '35_clm', '36_clm', '38_clm', '39_clm',
+         '43_clm', '46_clm', '47_clm', '48_clm', '50_clm', 'Aspect', 'dNBR', 'Elevation', 'LC', 'mNBR', 'pre', 'Slope',
+         'tpi']
+dfr = df.drop(tdrop, axis=1).reset_index(drop=True)
+
+# print(dfr.columns)
+# print(permutated.columns)
+
+dfc = pd.concat([dfr, permutated], axis=1)
+# dfc.to_csv(perm_path, index=False)
+
+# big no xy
+todrop = ['x', 'y']
+perm = df.drop(todrop, axis=1)
+permutated = perm.sample(frac=1, random_state=32).reset_index(drop=True)
+permutated.to_csv(datasets_path, index=False)
+
+# Print remaining columns and rows
+print(permutated.columns)
+print(permutated.eq(0).sum().sum())
+print(permutated.eq(np.nan).sum().sum())
+print(permutated.isnull().sum().sum())
+print(permutated.shape)
